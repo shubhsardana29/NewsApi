29b24893d4873b94197894dd9d424f19e0892233
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2023-05-12 00:25:34 +0530

    added cpi

--- lib/newscreen.dart
+++ lib/newscreen.dart
@@ -11,6 +11,7 @@ class NewsScreen extends StatefulWidget {
 
 class _NewsScreenState extends State<NewsScreen> {
   List<dynamic> articles = [];
+  bool isLoading = false;
 
   void launchURL(String url) async {
     if (await canLaunch(url)) {
@@ -21,19 +22,26 @@ class _NewsScreenState extends State<NewsScreen> {
   }
 
   Future<void> fetchNews() async {
+    setState(() {
+      isLoading = true;
+    });
     final response = await http.get(
-  Uri.parse('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=$newsApiKey'),
-);
-
+      Uri.parse(
+          'https://newsapi.org/v2/everything?domains=wsj.com&apiKey=$newsApiKey'),
+    );
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       setState(() {
         articles = data['articles'];
+        isLoading = false;
       });
     } else {
       // Handle error
       print('Error: ${response.statusCode}');
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 
@@ -43,28 +51,36 @@ class _NewsScreenState extends State<NewsScreen> {
       appBar: AppBar(
         title: Text('News'),
       ),
-      body: ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (context, index) {
-          final article = articles[index];
-          return ListTile(
-            title: Text(article['title']),
-            subtitle: Text(article['description']),
-            leading: article['urlToImage'] != null
-                ? Image.network(
-                    article['urlToImage'],
-                    width: 100,
-                    height: 100,
-                    fit: BoxFit.cover,
-                  )
-                : SizedBox.shrink(),
-            onTap: () {
-              if (article['url'] != null) {
-                launchURL(article['url']);
-              }
+      body: Stack(
+        children: [
+          ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              final article = articles[index];
+              return ListTile(
+                title: Text(article['title']),
+                subtitle: Text(article['description']),
+                leading: article['urlToImage'] != null
+                    ? Image.network(
+                        article['urlToImage'],
+                        width: 100,
+                        height: 100,
+                        fit: BoxFit.cover,
+                      )
+                    : SizedBox.shrink(),
+                onTap: () {
+                  if (article['url'] != null) {
+                    launchURL(article['url']);
+                  }
+                },
+              );
             },
-          );
-        },
+          ),
+          if (isLoading)
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: fetchNews,
